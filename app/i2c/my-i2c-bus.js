2f109ed31ea46c81f02d16b53eba1a831532a83c
Jeff Webster <jw7618@att.com>
2017-11-28 14:54:20 -0600

    Created new version of my-i2c-bus that creates ES6 promise wrappers for a subset of the i2c-bus functions. Added new mocha test files for both the standard i2c-bus functions that use async.js and my-i2c-bus that uses ES6 promises.

--- app/i2c/my-i2c-bus.js
+++ app/i2c/my-i2c-bus.js
@@ -1,558 +1,98 @@
-var fs = require('fs'),
-  i2c = require('bindings')('../../node_modules/i2c-bus/build/Release/i2c.node');
+/**
+ * Provides promise wrappers for subset of 12c-bus functions. Additional wrappers can be added as needed.
+ *
+ * Note that most of the functions return the bus object in the resolve object to make promise chaining
+ * a little easier.
+ */
 
-var DEVICE_PREFIX = '/dev/i2c-',
-  FIRST_SCAN_ADDR = 0x03,
-  LAST_SCAN_ADDR = 0x77;
-
-function Bus(busNumber, options) {
-  if (!(this instanceof Bus)) {
-    return new Bus(busNumber, options);
-  }
-  
-  options = options || {};
-  
-  this._busNumber = busNumber;
-  this._forceAccess = !!options.forceAccess || false;
-  this._peripherals = [];
-}
-
-function I2cFuncs(i2cFuncBits) {
-  if (!(this instanceof I2cFuncs)) {
-    return new I2cFuncs(i2cFuncBits);
-  }
-  
-  this.i2c = i2cFuncBits & i2c.I2C_FUNC_I2C;
-  this.tenBitAddr = i2cFuncBits & i2c.I2C_FUNC_10BIT_ADDR;
-  this.protocolMangling = i2cFuncBits & i2c.I2C_FUNC_PROTOCOL_MANGLING;
-  this.smbusPec = i2cFuncBits & i2c.I2C_FUNC_SMBUS_PEC;
-  this.smbusBlockProcCall = i2cFuncBits & i2c.I2C_FUNC_SMBUS_BLOCK_PROC_CALL;
-  this.smbusQuick = i2cFuncBits & i2c.I2C_FUNC_SMBUS_QUICK;
-  this.smbusReceiveByte = i2cFuncBits & i2c.I2C_FUNC_SMBUS_READ_BYTE;
-  this.smbusSendByte = i2cFuncBits & i2c.I2C_FUNC_SMBUS_WRITE_BYTE;
-  this.smbusReadByte = i2cFuncBits & i2c.I2C_FUNC_SMBUS_READ_BYTE_DATA;
-  this.smbusWriteByte = i2cFuncBits & i2c.I2C_FUNC_SMBUS_WRITE_BYTE_DATA;
-  this.smbusReadWord = i2cFuncBits & i2c.I2C_FUNC_SMBUS_READ_WORD_DATA;
-  this.smbusWriteWord = i2cFuncBits & i2c.I2C_FUNC_SMBUS_WRITE_WORD_DATA;
-  this.smbusProcCall = i2cFuncBits & i2c.I2C_FUNC_SMBUS_PROC_CALL;
-  this.smbusReadBlock = i2cFuncBits & i2c.I2C_FUNC_SMBUS_READ_BLOCK_DATA;
-  this.smbusWriteBlock = i2cFuncBits & i2c.I2C_FUNC_SMBUS_WRITE_BLOCK_DATA;
-  this.smbusReadI2cBlock = i2cFuncBits & i2c.I2C_FUNC_SMBUS_READ_I2C_BLOCK;
-  this.smbusWriteI2cBlock = i2cFuncBits & i2c.I2C_FUNC_SMBUS_WRITE_I2C_BLOCK;
-}
-
-function open(busNumber, options, cb) {
-  var bus;
-  
-  if (typeof options === 'function') {
-    cb = options;
-    options = undefined;
-  }
-  
-  bus = new Bus(busNumber, options);
-  
-  setImmediate(cb, null);
-  
-  return bus;
-}
-module.exports.open = open;
-
-function openSync(busNumber, options) {
-  return new Bus(busNumber, options);
-}
-module.exports.openSync = openSync;
-
-function peripheral(bus, addr, cb) {
-  var device = bus._peripherals[addr];
-  
-  if (device === undefined) {
-    fs.open(DEVICE_PREFIX + bus._busNumber, 'r+', function (err, device) {
-      if (err) {
-        return cb(err);
-      }
-      
-      bus._peripherals[addr] = device;
-      
-      i2c.setAddrAsync(device, addr, bus._forceAccess, function (err) {
-        if (err) {
-          return cb(err);
-        }
-        
-        cb(null, device);
-      });
-    });
-  } else {
-    setImmediate(cb, null, device);
-  }
-}
-
-function peripheralSync(bus, addr) {
-  var peripheral = bus._peripherals[addr];
-  
-  if (peripheral === undefined) {
-    peripheral = fs.openSync(DEVICE_PREFIX + bus._busNumber, 'r+');
-    bus._peripherals[addr] = peripheral;
-    i2c.setAddrSync(peripheral, addr, bus._forceAccess);
-  }
-  
-  return peripheral;
-}
-
-Bus.prototype.close = function (cb) {
-  var peripherals = this._peripherals.filter(function (peripheral) {
-    return peripheral !== undefined;
-  });
-  
-  (function close() {
-    if (peripherals.length === 0) {
-      return setImmediate(cb, null);
-    }
-    
-    fs.close(peripherals.pop(), function (err) {
-      if (err) {
-        return cb(err);
-      }
-      close();
-    });
-  }());
-};
-
-Bus.prototype.closeSync = function () {
-  this._peripherals.forEach(function (peripheral) {
-    if (peripheral !== undefined) {
-      fs.closeSync(peripheral);
-    }
-  });
-  this._peripherals = [];
-};
-
-Bus.prototype.i2cFuncs = function (cb) {
-  if (!this.funcs) {
-    peripheral(this, 0, function (err, device) {
-      if (err) {
-        return cb(err);
-      }
-      
-      i2c.i2cFuncsAsync(device, function (err, i2cFuncBits) {
-        if (err) {
-          return cb(err);
-        }
-        this.funcs = Object.freeze(new I2cFuncs(i2cFuncBits));
-        cb(null, this.funcs);
-      });
-    });
-  } else {
-    setImmediate(cb, null, this.funcs);
-  }
-};
-
-Bus.prototype.i2cFuncsSync = function () {
-  if (!this.funcs) {
-    this.funcs = Object.freeze(new I2cFuncs(i2c.i2cFuncsSync(peripheralSync(this, 0))));
-  }
-  
-  return this.funcs;
-};
-
-Bus.prototype.readByte = function (addr, cmd, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.readByteAsync(device, cmd, cb);
-  }.bind(this));
-};
-
-Bus.prototype.readByteSync = function (addr, cmd) {
-  return i2c.readByteSync(peripheralSync(this, addr), cmd);
-};
-
-Bus.prototype.readWord = function (addr, cmd, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.readWordAsync(device, cmd, cb);
-  }.bind(this));
-};
-
-Bus.prototype.readWordSync = function (addr, cmd) {
-  return i2c.readWordSync(peripheralSync(this, addr), cmd);
-};
-
-// UNTESTED and undocumented due to lack of supporting hardware
-Bus.prototype.readBlock = function (addr, cmd, buffer, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.readBlockAsync(device, cmd, buffer, cb);
-  }.bind(this));
-};
-
-// UNTESTED and undocumented due to lack of supporting hardware
-Bus.prototype.readBlockSync = function (addr, cmd, buffer) {
-  return i2c.readBlockSync(peripheralSync(this, addr), cmd, buffer);
-};
-
-Bus.prototype.readI2cBlock = function (addr, cmd, length, buffer, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.readI2cBlockAsync(device, cmd, length, buffer, cb);
-  }.bind(this));
-};
-
-Bus.prototype.readI2cBlockSync = function (addr, cmd, length, buffer) {
-  return i2c.readI2cBlockSync(peripheralSync(this, addr), cmd, length, buffer);
-};
-
-Bus.prototype.receiveByte = function (addr, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.receiveByteAsync(device, cb);
-  }.bind(this));
-};
-
-Bus.prototype.receiveByteSync = function (addr) {
-  return i2c.receiveByteSync(peripheralSync(this, addr));
-};
-
-Bus.prototype.sendByte = function (addr, byte, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.sendByteAsync(device, byte, cb);
-  }.bind(this));
-};
-
-Bus.prototype.sendByteSync = function (addr, byte) {
-  i2c.sendByteSync(peripheralSync(this, addr), byte);
-  return this;
-};
-
-Bus.prototype.writeByte = function (addr, cmd, byte, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.writeByteAsync(device, cmd, byte, cb);
-  }.bind(this));
-};
-
-Bus.prototype.writeByteSync = function (addr, cmd, byte) {
-  i2c.writeByteSync(peripheralSync(this, addr), cmd, byte);
-  return this;
-};
-
-Bus.prototype.writeWord = function (addr, cmd, word, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.writeWordAsync(device, cmd, word, cb);
-  }.bind(this));
-};
-
-Bus.prototype.writeWordSync = function (addr, cmd, word) {
-  i2c.writeWordSync(peripheralSync(this, addr), cmd, word);
-  return this;
-};
-
-Bus.prototype.writeQuick = function (addr, bit, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.writeQuickAsync(device, bit, cb);
-  }.bind(this));
-};
-
-Bus.prototype.writeQuickSync = function (addr, bit) {
-  i2c.writeQuickSync(peripheralSync(this, addr), bit);
-  return this;
-};
-
-// UNTESTED and undocumented due to lack of supporting hardware
-Bus.prototype.writeBlock = function (addr, cmd, length, buffer, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.writeBlockAsync(device, cmd, length, buffer, cb);
-  }.bind(this));
-};
-
-// UNTESTED and undocumented due to lack of supporting hardware
-Bus.prototype.writeBlockSync = function (addr, cmd, length, buffer) {
-  i2c.writeBlockSync(peripheralSync(this, addr), cmd, length, buffer);
-  return this;
-};
-
-Bus.prototype.writeI2cBlock = function (addr, cmd, length, buffer, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    i2c.writeI2cBlockAsync(device, cmd, length, buffer, cb);
-  }.bind(this));
-};
-
-Bus.prototype.writeI2cBlockSync = function (addr, cmd, length, buffer) {
-  i2c.writeI2cBlockSync(peripheralSync(this, addr), cmd, length, buffer);
-  return this;
-};
-
-Bus.prototype.i2cRead = function (addr, length, buffer, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    fs.read(device, buffer, 0, length, 0, cb);
-  }.bind(this));
-};
-
-Bus.prototype.i2cReadSync = function (addr, length, buffer) {
-  return fs.readSync(peripheralSync(this, addr), buffer, 0, length, 0);
-};
-
-Bus.prototype.i2cWrite = function (addr, length, buffer, cb) {
-  peripheral(this, addr, function (err, device) {
-    if (err) {
-      return cb(err);
-    }
-    
-    fs.write(device, buffer, 0, length, 0, cb);
-  }.bind(this));
-};
-
-Bus.prototype.i2cWriteSync = function (addr, length, buffer) {
-  return fs.writeSync(peripheralSync(this, addr), buffer, 0, length, 0);
-};
-
-Bus.prototype.scan = function (cb) {
-  var scanBus,
-    addresses = [];
-  
-  scanBus = open(this._busNumber, {forceAccess: this._forceAccess}, function (err) {
-    if (err) {
-      return cb(err);
-    }
-    
-    (function next(addr) {
-      if (addr > LAST_SCAN_ADDR) {
-        return scanBus.close(function (err) {
-          if (err) {
-            return cb(err);
-          }
-          cb(null, addresses);
-        });
-      }
-      
-      scanBus.receiveByte(addr, function (err) {
-        if (!err) {
-          addresses.push(addr);
-        }
-        
-        next(addr + 1);
-      });
-    }(FIRST_SCAN_ADDR));
-  });
-};
-
-Bus.prototype.scanSync = function () {
-  var scanBus = openSync(this._busNumber, {forceAccess: this._forceAccess}),
-    addresses = [],
-    addr;
-  
-  for (addr = FIRST_SCAN_ADDR; addr <= LAST_SCAN_ADDR; addr += 1) {
-    try {
-      scanBus.receiveByteSync(addr);
-      addresses.push(addr);
-    } catch (ignore) {
-    }
-  }
-  
-  scanBus.closeSync();
-  return addresses;
-};
-
-if ("win32" == process.platform) {
-  Bus = require('./win-i2c-bus.js').Bus;
-}
-
-/******************************************************************************
- Promise functions
- 
- Note: For some reason the original code uses a lot of recursive IIFE where
- looping constucts work just fine.
- I'm using loops instead unless a recursive IIFE is called for.
- ******************************************************************************/
-
-const util = require('util');
-
-const fsCloseP = util.promisify(fs.close);
+const i2c = require('i2c-bus');
 
 module.exports.openP = openP;
 
 function openP(busNumber, options) {
+  
   return new Promise(function (resolve, reject) {
-    resolve(new Bus(busNumber, options));
+    
+    let bus = i2c.open(busNumber, options, function (err) {
+      
+      if (!err) {
+        resolve(promisfyBus(bus));
+      } else {
+        reject(err);
+      }
+      
+    });
+    
+  });
+  
+};
+
+function promisfyBus(bus) {
+  
+  bus.closeP = closeP;
+  bus.scanP = scanP;
+  bus.i2cReadP = i2cReadP;
+  bus.i2cWriteP = i2cWriteP;
+  
+  return bus;
+}
+
+function closeP() {
+  
+  const self = this;
+  
+  return new Promise(function (resolve, reject) {
+    self.close(function (err) {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
   });
 }
 
-Bus.prototype.closeP = function () {
-  
-  // create array of pending promises to close all devices
-  const promises = this._peripherals.map((device) => {
-    if (device) {
-      return fsCloseP(device);
-    }
-  });
-  
-  return new Promise(function (resolve, reject) {
- 
-    // resolve all the pending "close device" promises
-    Promise.all(promises).then(() => {
-      resolve();
-    }).catch((err) => {
-      reject(err);
-    })
-  
-  });
-  
-};
-
-Bus.prototype.scanP = function () {
+function scanP() {
   
   const self = this;
   
   return new Promise(function (resolve, reject) {
-    
-    const devices = [];
-    
-    openP(self._busNumber, { forceAccess: self._forceAccess }).then((scanBus) => {
-      
-      (function next(addr) {
-        if (addr > LAST_SCAN_ADDR) {
-          return scanBus.close(function (err) {
-            if (err) {
-              reject(err);
-            }
-            //console.log('Finished scanning for devices');
-            resolve({bus: self, devices});
-          });
-        }
-        
-        scanBus.receiveByte(addr, function (err) {
-          if (!err) {
-            //console.log(`Found active devise at address=${addr}`);
-            devices.push(addr);
-          }
-          
-          next(addr + 1);
-        });
-      }(FIRST_SCAN_ADDR));
-      
-    }).catch((err) => {
-      reject(err);
-    });
-    
-  });
-  
-};
-
-Bus.prototype.i2cReadP = function (addr, length, buffer) {
-  
-  const self = this;
-  
-  return new Promise(function (resolve, reject) {
-  
-    peripheral(self, addr, function (err, device) {
-  
-      if (err) {
+    self.scan(function (err, devices) {
+      if (!err) {
+        resolve({bus: self, devices});
+      } else {
         reject(err);
       }
-  
-      fs.read(device, buffer, 0, length, 0, function (err, bytesRead, buffer) {
-    
-        if (!err) {
-          // Using ES6 object literal property shorthand
-          resolve({bus: self, bytesRead, buffer});
-        } else {
-          reject(err);
-        }
-    
-      });
     });
-    
   });
-  
-};
+}
 
-Bus.prototype.i2cWriteP = function (addr, length, buffer) {
+function i2cReadP(addr, length, buffer) {
   
   const self = this;
   
   return new Promise(function (resolve, reject) {
-  
-    peripheral(self, addr, function (err, device) {
-      
-      if (err) {
+    self.i2cRead(addr, length, buffer, function (err, bytesRead, buffer) {
+      if (!err) {
+        resolve({bus: self, bytesRead, buffer});
+      } else {
         reject(err);
       }
-    
-      fs.write(device, buffer, 0, length, 0, function (err, bytesWritten, buffer) {
-  
-        if (!err) {
-          // Using ES6 object literal property shorthand
-          resolve({bus: self, bytesWritten, buffer});
-        } else {
-          reject(err);
-        }
-        
-      });
     });
-
   });
-  
-};
+}
 
-Bus.prototype.receiveByteP = function (addr) {
+function i2cWriteP(addr, length, buffer) {
   
   const self = this;
   
   return new Promise(function (resolve, reject) {
-  
-    peripheral(self, addr, function (err, device) {
-      if (err) {
+    self.i2cWrite(addr, length, buffer, function (err, bytesWritten, buffer) {
+      if (!err) {
+        resolve({bus: self, bytesWritten, buffer});
+      } else {
         reject(err);
       }
-    
-      i2c.receiveByteAsync(device, function (err, byte) {
-        if (!err) {
-          resolve(byte);
-        } else {
-          reject(err);
-        }
-      });
     });
   });
-  
-};
+}
--- test/i2c/i2c-promise.test.js
+++ test/i2c/i2c-promise.test.js
@@ -1,15 +1,19 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-//const i2c = require('i2c-bus');
 const i2c = require('../../app/i2c/my-i2c-bus');
 
 const BUS_NUMBER = 1;
-const SEEDUINO_2_ADDRESS = 0x04;
-const SEEDUINO_2_REPLY = 'Slave2';
-const READ_BUFFER_SIZE = 6;
-const WRITE_BUFFER_SIZE = 1;
+const SEEDUINO_1_ADDRESS = 0x04;
+const SEEDUINO_1_REPLY = 'Slave0x04';
+const READ_BUFFER_SIZE = SEEDUINO_1_REPLY.length;
 const ACTIVE_DEVICES = 2;
+
+function pause(delay) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, delay);
+  });
+}
 
 describe('i2c', function () {
   
@@ -21,13 +25,15 @@ describe('i2c', function () {
       return bus.closeP();
     }).then(() => {
       expect(true).to.be.true;
+      return pause(100);
+    }).then(() => {
       done();
     }).catch((err) => {
       done(err);
     });
     
   });
-
+  
   it('scan i2c for active devices', function (done) {
 
     i2c.openP(BUS_NUMBER).then((bus) => {
@@ -36,6 +42,8 @@ describe('i2c', function () {
       expect(devices).to.have.lengthOf(ACTIVE_DEVICES);
       return bus.closeP();
     }).then(() => {
+      return pause(100);
+    }).then(() => {
       done();
     }).catch((err) => {
       done(err);
@@ -43,36 +51,41 @@ describe('i2c', function () {
 
   });
   
-  // it('request data from i2c slave', function (done) {
-  //
-  //   i2c.openP(BUS_NUMBER).then((bus) => {
-  //     const buffer = Buffer.alloc(READ_BUFFER_SIZE);
-  //     return bus.i2cReadP(SEEDUINO_2_ADDRESS, READ_BUFFER_SIZE, buffer);
-  //   }).then(({bus, bytesRead, buffer}) => { // using ES6 object destructuring
-  //     expect(buffer.toString()).to.equal(SEEDUINO_2_REPLY);
-  //     return bus.closeP();
-  //   }).then(() => {
-  //     done();
-  //   }).catch((err) => {
-  //     done(err);
-  //   });
-  //
-  // });
-  
-  it('send data to i2c slave', function (done) {
-    
+  it('request default response from i2c slave', function (done) {
+
     i2c.openP(BUS_NUMBER).then((bus) => {
-      const buffer = Buffer.from([0x01]);
-      return bus.i2cWriteP(SEEDUINO_2_ADDRESS, buffer.length, buffer);
-    }).then(({bus, bytesWritten, buffer}) => { // using ES6 object destructuring
-      expect(bytesWritten).to.equal(buffer.length);
+      const buffer = Buffer.alloc(READ_BUFFER_SIZE);
+      return bus.i2cReadP(SEEDUINO_1_ADDRESS, READ_BUFFER_SIZE, buffer);
+    }).then(({bus, bytesRead, buffer}) => { // using ES6 object destructuring
+      expect(buffer.toString()).to.equal(SEEDUINO_1_REPLY);
       return bus.closeP();
+    }).then(() => {
+      return pause(100);
     }).then(() => {
       done();
     }).catch((err) => {
       done(err);
     });
-    
+
   });
+  
+  it('send data to i2c slave', function (done) {
+
+    i2c.openP(BUS_NUMBER).then((bus) => {
+      const buffer = Buffer.from([0x01]);
+      return bus.i2cWriteP(SEEDUINO_1_ADDRESS, buffer.length, buffer);
+    }).then(({bus, bytesWritten, buffer}) => { // using ES6 object destructuring
+      expect(bytesWritten).to.equal(buffer.length);
+      return bus.closeP();
+    }).then(() => {
+      return pause(100);
+    }).then(() => {
+      done();
+    }).catch((err) => {
+      done(err);
+    });
+
+  });
+
   
 });
